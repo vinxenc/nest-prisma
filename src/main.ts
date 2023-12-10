8380e40e8e8516aa37722a59b8b56e4d7a3644c7
ductlv <aws.tutorial.vn@gmail.com>
2023-12-10 21:58:34 +0700

    chore: validate env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { env } from '@common';
 import { AppModule } from './app.module';
 
 require('module-alias/register');
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  await app.listen(3000);
+  await app.listen(env.PORT);
 }
 bootstrap();
